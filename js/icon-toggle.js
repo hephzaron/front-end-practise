bc9e217c4ca44f34305a6ec5ce8e11dc2f3c2d98
tobi.dara <tobi_daramola@yahoo.com>
2017-08-16 23:07:42 +0100

    modify view books page

---
+++ js/icon-toggle.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.collapse')
+        .on('shown.bs.collapse', function() {
+            $(this)
+                .parent().parent()
+                .find(".glyphicon-chevron-right")
+                .removeClass("glyphicon-chevron-right")
+                .addClass("glyphicon-chevron-down");
+        })
+        .on('hidden.bs.collapse', function() {
+            $(this)
+                .parent().parent()
+                .find(".glyphicon-chevron-down")
+                .removeClass("glyphicon-chevron-down")
+                .addClass("glyphicon-chevron-right");
+        });
+})
